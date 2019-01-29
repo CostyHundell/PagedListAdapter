2009b9808251bdbfc525a0eab8ac9520c731910a
Thomas Cundell <thomascundell@MacBook-Pro-4.fritz.box>
2019-01-29 15:10:14 +0100

    created observers in activity

--- app/src/main/java/com/thepeafactory/pagedlistadapterdemo/MainActivity.kt
+++ app/src/main/java/com/thepeafactory/pagedlistadapterdemo/MainActivity.kt
@@ -2,11 +2,30 @@ package com.thepeafactory.pagedlistadapterdemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val adapter = NewsPagedListAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        news_list_rv.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            this.adapter = adapter
+        }
+
+        ViewModelProviders.of(this).get(NewsListViewModel::class.java)
+            .getNewsList()
+            .observe(this, Observer { items ->
+                adapter.submitList(items)
+            })
+
     }
+
 }
